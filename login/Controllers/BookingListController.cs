e83a4f01d5d3e7a1934a75769c1e136ac3c8714e
Pradnya Mawal <pradnyadmawal@gmail.com>
2020-01-30 12:29:43 +0530

    firstCommit

---
+++ login/Controllers/BookingListController.cs
@@ -0,0 +1,51 @@
+﻿using login.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace login.Controllers
+{
+    public class BookingListController : Controller
+    {
+        // GET: BookingList
+        public ActionResult OwnerView()
+        {
+            return View();
+        }
+        public ActionResult BookingHistory()
+        {
+            List<Booking> clist = new List<Booking>();
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=project;Integrated Security=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select client.name as CName, owner.Name as OName ,BookingId,Date from client join Booking on Client.Cid = Booking.Cid join owner on Booking.Oid =Owner.Oid";
+
+            SqlDataReader dr = cmd.ExecuteReader();
+           
+
+            while (dr.Read())
+            {
+                Booking o = new Booking();
+                o.CName = Convert.ToString(dr["CName"]);
+                o.OName = Convert.ToString(dr["OName"]);
+                o.BookingId = Convert.ToInt32(dr["BookingId"]);
+                o.Date = Convert.ToString(dr["Date"]);
+                clist.Add(o);
+            }
+
+
+            return View(clist);
+        }
+
+
+        // GET: BookingList
+
+    }
+}
---
+++ login/Models/Client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace login.Models
+{
+    public class Client
+    {
+        [key]
+        
+        public int Cid { get; set; }
+
+        [Required(ErrorMessage = "Enter Your Name")]
+        [RegularExpression("^[a-zA-Z0-9@$]*", ErrorMessage = "Only Alphabets are allowed")]
+        [DisplayName("Client Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Email-Id")]
+        [Required(ErrorMessage = "This field is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password length should be minimum 4 and maximum 8"), MaxLength(8), MinLength(4)]
+        [RegularExpression("^[a-zA-Z0-9@$]*", ErrorMessage = "Only Alphabets, Numbers and @, $ are allowed.")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DisplayName("Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        [Required(ErrorMessage = "password not match")]
+        public string ConfirmPassword { get; set; }
+
+
+        
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression("^[0-9]*", ErrorMessage = "Only 10 digits Numbers are allowed.")]
+        [Required(ErrorMessage = "Enter valid phone number")]
+        public long Phone { get; set; }
+
+        public bool Membership { get; set; }
+        
+        [Required(ErrorMessage = "Enter your age")]
+        [Range(15, 100)]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Gender { get; set; }
+
+
+    }
+}
